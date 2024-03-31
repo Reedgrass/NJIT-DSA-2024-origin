6dba0082224a292f87770b937b360724e582ccb1
Reedgrass <126428835+Reedgrass@users.noreply.github.com>
2024-03-31 22:47:45 +0800

    Update Grades.java

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,3 +1,4 @@
+
 package oy.tol.tra;
 
 public class Grades {
@@ -13,61 +14,15 @@ public class Grades {
 
    public void reverse() {
 
-      int i = 0;
-      while (i < grades.length / 2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length - i - 1];
-         grades[grades.length - i - 1] = temp;
-         i++;
-      }
+      Algorithms.reverse(this.grades);
    }
 
-   // public void sort() {
-
-   // int i = grades.length - 1;
-   // while (i > 0) {
-   // if (grades[i] < grades[i - 1]) {
-   // int tmp = grades[i];
-   // grades[i] = grades[i - 1];
-   // grades[i - 1] = tmp;
-   // }
-   // i--;
-   // }
-   // }
    public void sort() {
-      int n = grades.length;
-      for (int i = 0; i < n - 1; i++) {
-         for (int j = 0; j < n - i - 1; j++) {
-            if (grades[j] > grades[j + 1]) {
-               // Swap grades[j] and grades[j+1]
-               int temp = grades[j];
-               grades[j] = grades[j + 1];
-               grades[j + 1] = temp;
-            }
-         }
-      }
-   }
 
-   // // public void sort() {
-   // // boolean swapped;
-   // // for (int i = grades.length - 1; i > 0; i--) {
-   // // swapped = false;
-   // // for (int j = 0; j < i; j++) {
-   // // if (grades[j] > grades[j + 1]) {
-   // // int tmp = grades[j];
-   // // grades[j] = grades[j + 1];
-   // // grades[j + 1] = tmp;
-   // // swapped = true;
-   // // }
-   // // }
-   // // if (!swapped) {
-   // // break;
-   // // }
-   // // }
-   // // }
+      Algorithms.sort(this.grades);
+   }
 
    public Integer[] getArray() {
       return grades;
    }
 }
-
