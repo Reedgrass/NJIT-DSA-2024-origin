d42a0430561ee6f7f8918ef535dbbcfec9b9c77c
Reedgrass <126428835+Reedgrass@users.noreply.github.com>
2024-03-31 22:47:16 +0800

    Update Algorithms.java

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,35 +1,29 @@
 package oy.tol.tra;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Algorithms {
 
     public static <T> void reverse(T[] array) {
         int i = 0;
-        while (i < array.length / 2) {
-            T temp = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i - 1] = temp;
+        int j = array.length - 1;
+        while (i < j) {
+            swap(array, i, j);
             i++;
+            j--;
         }
     }
 
     public static <T extends Comparable<T>> void sort(T[] array) {
         int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
                 if (array[j].compareTo(array[j + 1]) > 0) {
-                    // Swap array[j] and array[j+1]
-                    T temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    swap(array, j, j + 1);
                 }
-            }
-        }
-
     }
 
+    public static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
